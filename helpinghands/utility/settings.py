d22961a497e69ae109545f9284de55194582ef40
Emil Rühmland <emilrueh@googlemail.com>
2023-08-08 18:18:23 +0200

    Refactor load_settings()
    Added programmatical approach to secrets keys and allowing Docker environment variables

--- helpinghands/utility/settings.py
+++ helpinghands/utility/settings.py
@@ -9,42 +9,58 @@ import json
 from termcolor import colored
 
 
-def load_settings(settings_path, dotenv_path=None):
+def load_settings(
+    dotenv_settings_path="SETTINGS_PATH",
+    secrets_keys_list=None,
+    dotenv_path=None,
+    remote_env="DOCKER_ENV",
+    default_settings_path="data/settings.json",
+):
     # Initialize an empty dictionary for secrets
     secrets_dict = {}
 
-    print(colored(f"Loading settings from {settings_path}", "cyan"))
+    try:
+        if os.getenv(remote_env) is None:
+            if dotenv_path:
+                load_dotenv(dotenv_path)
+            else:
+                load_dotenv()
+            logger.debug(colored("Loaded local .env file", "cyan"))
 
-    # Load .env variables if load_secrets is True
-    if dotenv_path:
-        print(colored(f"Loading secrets from {dotenv_path}", "cyan"))
-        load_dotenv(dotenv_path)
+        # Load .env variables if keys are provided
+        if secrets_keys_list:
+            for secret_key in secrets_keys_list:
+                value = os.getenv(secret_key)
+                if value:
+                    secrets_dict[secret_key] = value
+                else:
+                    logger.warning(f"Missing environment variable: {secret_key}")
 
-        secrets_dict = {
-            "AIRTABLE_API_TOKEN": os.getenv("AIRTABLE_API_TOKEN"),
-            "OPENAI_API_KEY": os.getenv("OPENAI_API_KEY"),
-            "DALLEE_API_KEY": os.getenv("DALLEE_API_KEY"),
-            "EVENTBRITE_PRIVATE_TOKEN": os.getenv("EVENTBRITE_PRIVATE_TOKEN"),
-            "XANO_API_KEY": os.getenv("XANO_API_KEY"),
-            "XANO_ENDPOINT_POST": os.getenv("XANO_ENDPOINT_POST"),
-            "XANO_ENDPOINT_IMAGE": os.getenv("XANO_ENDPOINT_IMAGE"),
-            "XANO_ENDPOINT_GET_ALL": os.getenv("XANO_ENDPOINT_GET_ALL"),
-            "XANO_ENDPOINT_EDIT": os.getenv("XANO_ENDPOINT_EDIT"),
-            "XANO_ENDPOINT_DELETE": os.getenv("XANO_ENDPOINT_DELETE"),
-            "XANO_TABLE_NAME": os.getenv("XANO_TABLE_NAME"),
-            "LINKEDIN_USER": os.getenv("LINKEDIN_USER"),
-            "LINKEDIN_PWORD": os.getenv("LINKEDIN_PWORD"),
-        }
+            logger.debug(
+                colored(f"Loaded secrets: {', '.join(secrets_keys_list)}", "cyan")
+            )
 
-    # Check if the settings file exists
-    if not os.path.exists(settings_path):
-        raise FileNotFoundError(f"Settings file not found: {settings_path}")
+        # Fetch settings path either from the environment or use the default one
+        settings_path = os.getenv(dotenv_settings_path, default_settings_path)
 
-    # Load the settings dictionary from the .json file
-    with open(settings_path, "r") as fp:
-        settings_dict = json.load(fp)
+        # Check if the settings file exists
+        if not os.path.exists(settings_path):
+            raise FileNotFoundError(f"Settings file not found: {settings_path}")
 
-    # Merge the API keys and other settings
-    settings_dict.update(secrets_dict)
+        logger.info(colored(f"Loading settings from {settings_path}", "cyan"))
 
-    return settings_dict
+        # Load the settings dictionary from the .json file
+        with open(settings_path, "r") as fp:
+            settings_dict = json.load(fp)
+
+        # Merge the API keys and other settings
+        settings_dict.update(secrets_dict)
+
+        return settings_dict
+
+    except json.JSONDecodeError:
+        logger.error(f"Error decoding JSON from settings file: {settings_path}")
+        raise
+    except Exception as e:
+        logger.error(f"Unexpected {type(e).__name__}: {e}")
+        raise
