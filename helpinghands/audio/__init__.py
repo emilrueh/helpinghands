ac875e39992d027638cf5ac0ee117ecfba26a50a
Emil Rühmland <emilrueh@googlemail.com>
2023-08-15 13:53:58 +0200

    Account for audio imports failing

--- helpinghands/audio/__init__.py
+++ helpinghands/audio/__init__.py
@@ -1,3 +1,10 @@
-from .sounds import uhoh, criterr, warning, success
-from .converter import ogg_to_mp3
-from .recorder import AudioRecorder
+try:
+    from .sounds import uhoh, criterr, warning, success
+    from .converter import ogg_to_mp3
+    from .recorder import AudioRecorder
+except (ImportError, OSError):
+    import warnings
+
+    warnings.warn(
+        "The audio module is disabled in the current env due to missing dependencies. All other helpinghands modules should work as expected."
+    )
