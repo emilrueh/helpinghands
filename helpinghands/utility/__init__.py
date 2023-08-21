01a32218c8d169ff530c0cc83503c592309ca29f
Emil Rühmland <emilrueh@googlemail.com>
2023-08-21 14:30:26 +0200

    Conditional web.py imports

--- helpinghands/utility/__init__.py
+++ helpinghands/utility/__init__.py
@@ -27,18 +27,26 @@ from .tokenbucket import TokenBucket
 
 from .decorator import retry, time_execution
 from .helper import get_git_tree, colorize, get_variable_name, ensure_windows_os
-from .web import (
-    WebConfig,
-    setup_browser,
-    get_website,
-    setup_proxy_simple,
-    setup_proxy_wire,
-    test_proxy,
-    get_original_ip,
-    get_current_ip,
-    rotate_ip,
-    make_soup,
-    connect_to_vpn,
-    disconnect_from_vpn,
-    check_internet,
-)
+
+try:
+    from .web import (
+        WebConfig,
+        setup_browser,
+        get_website,
+        setup_proxy_simple,
+        setup_proxy_wire,
+        test_proxy,
+        get_original_ip,
+        get_current_ip,
+        rotate_ip,
+        make_soup,
+        connect_to_vpn,
+        disconnect_from_vpn,
+        check_internet,
+    )
+except ModuleNotFoundError:
+    import warnings
+
+    warnings.warn(
+        "The web.py module is disabled in the current env due to missing dependencies. All other helpinghands modules should work as expected."
+    )
