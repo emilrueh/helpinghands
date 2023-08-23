8d8df46576daefb15bd56716f491eea9ea57ec83
Emil Rühmland <emilrueh@googlemail.com>
2023-08-24 01:10:19 +0200

    Add convert_audio()

--- helpinghands/audio/__init__.py
+++ helpinghands/audio/__init__.py
@@ -1,6 +1,6 @@
 try:
     from .sounds import uhoh, criterr, warning, success
-    from .converter import ogg_to_mp3
+    from .converter import convert_audio, combine_audio_files
     from .recorder import AudioRecorder
 except (ImportError, OSError):
     import warnings
