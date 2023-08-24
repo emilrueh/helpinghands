e814766606502aa8676c57a742a158c9fa752419
Emil Rühmland <emilrueh@googlemail.com>
2023-08-25 01:00:48 +0200

    Import image funcs

--- helpinghands/ai/__init__.py
+++ helpinghands/ai/__init__.py
@@ -1,4 +1,4 @@
 from .whisper import call_whisper
 from .gpt import call_gpt, gpt_loop
 from .dallee import generate_image, dallee_loop
-from .upscale import super_image
+from .upscale import super_image, super_image_loop
