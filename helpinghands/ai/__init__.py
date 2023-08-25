fc83deb0ab4f8a9069f4458eac6b0771a4c69aa5
Emil Rühmland <emilrueh@googlemail.com>
2023-08-25 17:40:41 +0200

    Implement upscale import condition

--- helpinghands/ai/__init__.py
+++ helpinghands/ai/__init__.py
@@ -1,4 +1,10 @@
 from .whisper import call_whisper
 from .gpt import call_gpt, gpt_loop
 from .dallee import generate_image, dallee_loop
-from .upscale import super_image, super_image_loop
+
+try:
+    from .upscale import super_image, super_image_loop
+except ModuleNotFoundError:
+    import warnings
+
+    warnings.warn("'ai.upscale' is disabled")
