a1dd0231f2123e2f4dc654ff82490320222df63e
Emil Rühmland <emilrueh@googlemail.com>
2023-08-28 15:53:48 +0200

    Refactor for better exception handling

--- helpinghands/audio/__init__.py
+++ helpinghands/audio/__init__.py
@@ -1,8 +1,8 @@
 try:
     from .sounds import uhoh, criterr, warning, success
     from .converter import convert_audio, combine_audio_files
-    from .recorder import AudioRecorder
-except (ImportError, OSError):
+    from .recorder import AudioRecorder, ContinuousRecorder
+except (ImportError, OSError) as e:
     import warnings
 
-    warnings.warn("'audio' is disabled")
+    warnings.warn(f"'audio' is disabled ({type(e).__name__}{e})")
