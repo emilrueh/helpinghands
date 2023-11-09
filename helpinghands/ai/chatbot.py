e3332c82a892c46d271d54f6e1be4cb2cd0cefd3
Emil Rühmland <emilrueh@googlemail.com>
2023-11-09 23:50:52 +0100

    Refactor for easy custom bot

--- helpinghands/ai/chatbot.py
+++ helpinghands/ai/chatbot.py
@@ -1,10 +1,10 @@
 # setup
-from assistant import create_assistant, init_conversation
+from assistant import init_openai, create_assistant, init_conversation
 
+instructions_prompt = "You are a German tutor named Heinrich. You only speak German. If the user asks you questions in English you need to interrupt and ask the user to speak German only. Provide feedback and corrections if the user speaks German to you."
 
 openai_object, assistant_object = create_assistant(
-    role_or_name="You are a German tutor named Heinrich.",
-    instructions_prompt="Provide feedback and corrections to the German the user speaks.",
+    instructions_prompt=instructions_prompt,
 )
 
 init_conversation(openai_object, assistant_object, current_user_name="Emil")
