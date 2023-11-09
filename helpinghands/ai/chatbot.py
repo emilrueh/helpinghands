14937fcd16ff37edb8bbc3df8023969f8d1567d4
Emil Rühmland <emilrueh@googlemail.com>
2023-11-09 23:22:36 +0100

    Add custom args

--- helpinghands/ai/chatbot.py
+++ helpinghands/ai/chatbot.py
@@ -2,6 +2,9 @@
 from assistant import create_assistant, init_conversation
 
 
-openai_object, assistant_object = create_assistant()
+openai_object, assistant_object = create_assistant(
+    role_or_name="You are a German tutor named Heinrich.",
+    instructions_prompt="Provide feedback and corrections to the German the user speaks.",
+)
 
-init_conversation(openai_object, assistant_object)
+init_conversation(openai_object, assistant_object, current_user_name="Emil")
