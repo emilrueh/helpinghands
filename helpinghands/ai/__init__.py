9bee5c5df58647adcd46e681759567873f21b6a4
Emil Rühmland <emilrueh@googlemail.com>
2023-11-09 21:53:23 +0100

    Add chatbot

--- helpinghands/ai/__init__.py
+++ helpinghands/ai/__init__.py
@@ -2,6 +2,7 @@ from .whisper import call_whisper
 from .gpt import call_gpt, gpt_loop
 from .dallee import generate_image, dallee_loop
 from .assistant import talk_to_assistant
+from .chatbot import activate_chatbot
 
 try:
     from .upscale import super_image, super_image_loop
