ef9a395b756e2dbef7163a552fe702c61682dbfc
Emil Rühmland <emilrueh@googlemail.com>
2023-11-10 14:17:48 +0100

    Refactor for client and obj

--- helpinghands/ai/chatbot.py
+++ helpinghands/ai/chatbot.py
@@ -5,20 +5,20 @@ from assistant import create_assistant, create_thread, init_conversation
 instructions_prompt = "You are a German tutor named Heinrich. You only speak German. Provide feedback and corrections when the user speaks German to you. If the user asks you questions in English you need to interrupt and ask the user to speak German only."
 
 # setup openai assistant
-openai_object, assistant_object = create_assistant(instructions_prompt)
+openai_client, assistant_obj = create_assistant(instructions_prompt)
 
 # create thread (conversation)
-thread_object = create_thread(openai_object)
+thread_obj = create_thread(openai_client)
 
 # initialize conversation (thread)
 init_conversation(
-    openai_object,
-    assistant_object,
-    thread_object,
+    openai_client,
+    assistant_obj,
+    thread_obj,
     current_user_name="Emil",
     run_instructions=None,
 )
 
 # store for later processing
-current_assistant = assistant_object.id
-current_thread = thread_object.id
+current_assistant = assistant_obj.id
+current_thread = thread_obj.id
