8c6b2c5dfedfbf577cd715144a22a8dd278a6392
Emil Rühmland <emilrueh@googlemail.com>
2023-11-11 23:52:21 +0100

    Continue when no image

--- helpinghands/ai/vision.py
+++ helpinghands/ai/vision.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 from dotenv import load_dotenv
+import os
 
 load_dotenv()
 
@@ -16,7 +17,7 @@ from io import BytesIO
 import requests
 
 
-def image_path_to_base64str(image_source, file_type="JPEG"):
+def image_to_base64str(image_source, file_type="JPEG"):
     if "http" in image_source or "https" in image_source:
         # Handle URL
         response = requests.get(image_source)
@@ -67,9 +68,10 @@ def view_image(images_in_base64str: list, prompt, max_tokens=300):
             "content": [{"type": "text", "text": prompt}],
         },
     ]
-    print("messages:\n", messages)
 
     for img in images_in_base64str:
+        if img is None:
+            continue
         messages[0]["content"].append(
             {
                 "type": "image_url",
@@ -77,7 +79,6 @@ def view_image(images_in_base64str: list, prompt, max_tokens=300):
             },
         )
 
-    print("messages:\n", len(messages[0].items()))
     response = client.chat.completions.create(
         model="gpt-4-vision-preview",
         messages=messages,
@@ -87,48 +88,3 @@ def view_image(images_in_base64str: list, prompt, max_tokens=300):
 
 
 # ---
-
-"""
-Things wrong:
-
-    - the output images need to be saved to files
-        - in the function or outside?
-
-    - the image_generated need to be base64string from url
-        - download and transform (in the function or outside?)
-"""
-
-# def image_generation_iteration(image: str, iterations: int = 3, directory: str = None):
-#     image_generated = None
-#     prompt_generate = None
-
-#     if
-
-#     for i in range(iterations):
-#         if not image_generated:
-#             # original image
-#             original_image_b64str = vision.image_path_to_base64str(image)
-#             initial_prompt = f"Your task is to write a highly detailed description of the picture. Make the reader feel like being there."
-
-#             # setting which prompt to use
-#             recurring_prompt = f"I have written a prompt for the AI art generation model Dalle 3 to replicate the original image. Your task is to fine tune the prompt to match the original image as closely as possible. Only respond with the new prompt."
-#             recurring_prompt = (
-#                 recurring_prompt + f"Here is the original prompt: {prompt_generate}"
-#                 if not image_generated
-#                 else recurring_prompt
-#             )
-#             # use initial prompt for the first iteration
-#             prompt_for_view_image = (
-#                 recurring_prompt if image_generated else initial_prompt
-#             )
-#             images_to_analyze = [original_image_b64str, image_generated_url]
-#             # analyze image
-#             prompt_generate = vision.view_image(images_to_analyze
-#                 , prompt_for_view_image
-#             )
-
-#             # generate image
-#             image_generated_url = vision.generate_image(prompt_generate)
-
-
-#             return image_generated_url
