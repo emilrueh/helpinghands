613db83cfedf83cae77323f9c344ebbc6db99ade
Emil Rühmland <emilrueh@googlemail.com>
2023-11-24 16:06:04 +0100

    Refactor of text funcs to text.py

---
+++ helpinghands/utility/text.py
@@ -0,0 +1,51 @@
+from ..utility.logger import get_logger
+
+import os, subprocess, textwrap, platform
+
+
+# TXT
+def split_path(file_path):
+    directory = os.path.dirname(file_path)
+    split_name = os.path.splitext(os.path.basename(file_path))
+    filename_without_extension = split_name[0]
+    extension = split_name[1]
+    return directory, filename_without_extension, extension
+
+
+def insert_newlines(string, every=64):
+    logger = get_logger()
+    logger.info(f"Formatted string.")
+    return "\n".join(textwrap.wrap(string, every))
+
+
+def write_to_txt_file(
+    input_text, file_name, output_directory, mode="append", encoding="utf-8"
+):
+    """
+    Specify any mode except for 'append' for write and replace.
+    """
+    output_file_path = os.path.join(output_directory, f"{file_name}.txt")
+    with open(
+        output_file_path, "a" if mode == "append" else "w", encoding=encoding
+    ) as f:
+        f.write(("\n" if mode == "append" and f.tell() > 0 else "") + input_text)
+
+
+def load_text_from_file(txt_file_path):
+    try:
+        with open(txt_file_path, "r") as f:
+            return f.read()
+    except:
+        return print(f"Failed to open .txt file at path: {txt_file_path}")
+
+
+def open_txt_file(file_path):
+    try:
+        if platform.system() == "Windows":
+            os.startfile(file_path)
+        elif platform.system() == "Darwin":  # macOS
+            subprocess.run(["open", file_path], check=True)
+        elif platform.system() == "Linux":
+            subprocess.run(["xdg-open", file_path], check=True)
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
